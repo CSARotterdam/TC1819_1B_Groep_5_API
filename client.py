7da43a58d8aa56ae8956679ef9963fe855062a10
TehNolz <36479504+TehNolz@users.noreply.github.com>
2019-03-28 10:42:05 +0100

    update gitignore, add local ipv4 auto-detect

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 while True:
-	r = requests.post("http://192.168.178.9", json={"hello": "world"})
+	r = requests.post("http://localhost", json={"hello": "world"})
 	print(r.status_code)
 	print(r.text["goodbye"])
 	input()
