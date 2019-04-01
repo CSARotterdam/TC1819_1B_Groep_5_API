8320890d76165a501266290d088702fb5c4f1044
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-04-01 19:49:42 +0200

    Add config file

---
+++ Web API/Config.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Web_API {
+	class Config {
+		private static string defaultJson = @"{
+			'databaseSettings': {
+				'username': null,
+				'password': null,
+				'serverAddress': null
+			},
+			'connectionSettings': {
+				'autodetect': true,
+				'serverAddresses': [
+					'localhost'
+				]
+			},
+			'performanceSettings':{
+				'workerThreadCount': 5,
+			}
+		}";
+
+		public static dynamic loadConfig(){
+			string filename = "config.json";
+			dynamic Settings;
+
+			if (!File.Exists(filename)){
+				Settings = JObject.Parse(defaultJson);
+				using (JsonTextWriter writer = new JsonTextWriter(File.CreateText(filename))) {
+					writer.Formatting = Formatting.Indented;
+					Settings.WriteTo(writer);
+				}
+
+			} else {
+				Settings = JObject.Parse(File.ReadAllText(filename));
+			}
+			return Settings;
+		}
+	}
+}
--- Web API/Program.cs
+++ Web API/Program.cs
@@ -1,64 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Net.Sockets;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 using System.Threading;
-using MySQLWrapper;
-using System.IO;
-using System.Configuration;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Web_API {
 	class Program {
 		public static void Main() {
-			// Get local IP address
-			String address;
-			using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
-				socket.Connect("8.8.8.8", 65530);
-				IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-				address = endPoint.Address.ToString();
+			//Load configuration file
+			dynamic Settings = Config.loadConfig();
+
+			//Check if config contains all necessary info to start. If it doesn't, abort launch.
+			bool validConfig = true;
+			if(Settings.databaseSettings.username == null || Settings.databaseSettings.password == null || Settings.databaseSettings.serverAddress == null) {
+				Console.WriteLine("Error: Incomplete database configuration.");
+				validConfig = false;
 			}
-			Console.WriteLine("Local IP address is "+ address);
-
-			//Create request queue
-			BlockingCollection<HttpListenerContext> requestQueue = new BlockingCollection<HttpListenerContext>();
-
-			//Create console command thread
-			Thread consoleThread = new Thread(() => ConsoleCommand.main());
-			consoleThread.Start();
-
-			//Create database connection
-			Thread databaseMaintainerThread = new Thread(() => DatabaseMaintainer.main());
-			databaseMaintainerThread.Start();
-
-			//Create worker threads
-			Console.WriteLine("Creating worker threads.");
-			int threadCount = 5;
-			Thread[] threadList = new Thread[threadCount];
-			for (int i = 0; i != threadCount; i++) {
-				Thread workerThread = new Thread(() => RequestWorker.main(requestQueue));
-				workerThread.Name = "WorkerThread" + i.ToString();
-				workerThread.Start();
-				threadList[i] = workerThread;
+			if((bool)Settings.connectionSettings.autodetect && Settings.connectionSettings.serverAddresses.Count == 0) {
+				Console.WriteLine("Error: Missing server address.");
+				validConfig = false;
 			}
-			Console.WriteLine("Finished creating worker threads.");
+		
+			if (!validConfig) {
+				Console.WriteLine("The server failed to start because some required data is missing. Please check the configuration!");
+				Console.ReadLine();
+			} else {
+				//Get local IP address, if autodetect is enabled in settings.
+				List<string> addresses = Settings.connectionSettings.serverAddresses.ToObject<List<string>>();
+				if ((bool)Settings.connectionSettings.autodetect) {
+					using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
+						socket.Connect("8.8.8.8", 65530);
+						IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+						string address = endPoint.Address.ToString();
+						addresses.Add(address);
+					}
+				}
 
-			// Create listener thingy.
-			HttpListener listener = new HttpListener();
-			listener.Prefixes.Add("http://localhost/");
-			listener.Prefixes.Add("http://" + address + "/");
-			listener.Start();
-			System.Console.WriteLine("Now listening for requests.");
+				//Create request queue
+				BlockingCollection<HttpListenerContext> requestQueue = new BlockingCollection<HttpListenerContext>();
 
-			// Main loop
-			while (true) {
-				// Wait for request 
-				requestQueue.Add(listener.GetContext());
-				Console.WriteLine("Received and enqueued a request!");
+				//Create console command thread
+				Thread consoleThread = new Thread(() => ConsoleCommand.main());
+				consoleThread.Start();
+
+				//Create database maintainer thread
+				Thread databaseMaintainerThread = new Thread(() => DatabaseMaintainer.main());
+				databaseMaintainerThread.Start();
+
+				//Create worker threads
+				Console.WriteLine("Creating worker threads.");
+				int threadCount = (int)Settings.performanceSettings.workerThreadCount;
+				Thread[] threadList = new Thread[threadCount];
+				for (int i = 0; i != threadCount; i++) {
+					Thread workerThread = new Thread(() => RequestWorker.main(requestQueue));
+					workerThread.Name = "WorkerThread" + i.ToString();
+					workerThread.Start();
+					threadList[i] = workerThread;
+				}
+				Console.WriteLine("Finished creating worker threads.");
+
+				// Create listener thingy.
+				HttpListener listener = new HttpListener();
+				foreach (string address in addresses) {
+					listener.Prefixes.Add("http://" + address + "/");
+				}
+				listener.Start();
+				System.Console.WriteLine("Now listening for requests.");
+
+				// Main loop
+				while (true) {
+					// Wait for request 
+					requestQueue.Add(listener.GetContext());
+					Console.WriteLine("Received and enqueued a request!");
+				}
 			}
 		}
 	}
