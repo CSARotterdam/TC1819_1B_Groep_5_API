3133e1047b8d0f05fe2e45c96e880ef6aac63eb5
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-04-01 22:15:59 +0200

    Changed .::. TechlabMySQL now builds it's connectionString with a MySqlConnectionStringBuilder object and allows for greater control over the connection string.

--- Web API/SQL/SQLConstants.cs
+++ Web API/SQL/SQLConstants.cs
@@ -7,14 +7,6 @@ namespace MySQLWrapper.MySQL
 {
 	static class SQLConstants
 	{
-		public const string ServerUrl = "persist security info=<persist>;" +
-			"server=sql.hosted.hr.nl;" +
-			"port=3306;" +
-			"user=<username>;" +
-			"password=<password>;" +
-			"database=<database>;" +
-			"connection timeout=<timeout>";
-
 		public const string SelectForeignKeys = "SELECT TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME " +
 			"FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_SCHEMA = DATABASE() AND TABLE_NAME = <table>";
 
