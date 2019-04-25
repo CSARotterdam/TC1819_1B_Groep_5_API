5abe2fc91dd71e9528cd4f1e6b40c9ace96e52b9
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-04-25 18:09:54 +0200

    Username and password are now retrieved from the database. Yay!

--- Web API/Requests/Misc.cs
+++ Web API/Requests/Misc.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using MySQLWrapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace API.Requests {
 	static partial class RequestMethods {
-		
+		public static TechlabMySQL wrapper;
 	}
 }
