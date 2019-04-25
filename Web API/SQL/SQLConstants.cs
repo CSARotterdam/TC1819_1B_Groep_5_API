21e970f3bbaba1b9e74798b69ec8762b720a55c8
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-04-25 22:47:24 +0200

    Added comments and documentation.

--- Web API/SQL/SQLConstants.cs
+++ Web API/SQL/SQLConstants.cs
@@ -7,13 +7,28 @@
 		public const string Update = "UPDATE <schema> SET <column_value_pairs> WHERE <condition>";
 		public const string Delete = "DELETE FROM <schema> WHERE <condition>";
 
+		/// <summary>
+		/// Builds and return a SELECT sql query.
+		/// </summary>
 		public static string GetSelect(string columns, string schema, string condition) =>
 			Select.Replace("<condition>", condition).Replace("<schema>", schema).Replace("<columns>", columns);
+		/// <summary>
+		/// Builds and return a SELECT sql query.
+		/// </summary>
 		public static string GetSelect(string columns, string schema, string condition, long limit) => GetSelect(columns, schema, condition) + " LIMIT " + limit;
+		/// <summary>
+		/// Builds and return a INSERT sql command.
+		/// </summary>
 		public static string GetInsert(string schema, string columns, string values) =>
 			Insert.Replace("<values>", values).Replace("<columns>", columns).Replace("<schema>", schema);
+		/// <summary>
+		/// Builds and return a UPDATE sql command.
+		/// </summary>
 		public static string GetUpdate(string schema, string column_value_pairs, string condition) =>
 			Update.Replace("<condition>", condition).Replace("<column_value_pairs>", column_value_pairs).Replace("<schema>", schema);
+		/// <summary>
+		/// Builds and return a DELETE sql command.
+		/// </summary>
 		public static string GetDelete(string schema, string condition) =>
 			Delete.Replace("<condition>", condition).Replace("<schema>", schema);
 	}
