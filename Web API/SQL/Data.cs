43cafcc6a10469f84ad16b7310863ebb51a22dc3
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-05-14 11:50:40 +0200

    i feex - Part 2: The Electric Boogaloo

--- Web API/SQL/Data.cs
+++ Web API/SQL/Data.cs
@@ -495,7 +495,7 @@ namespace MySQLWrapper.Data
 		{
 			new ColumnMetadata("id", 50, MySqlDbType.VarChar),
 			new ColumnMetadata("manufacturer", 80, MySqlDbType.VarChar),
-			new ColumnMetadata("category", 11, MySqlDbType.Int32),
+			new ColumnMetadata("category", 50, MySqlDbType.VarChar),
 			new ColumnMetadata("name", 50, MySqlDbType.VarChar),
 			new ColumnMetadata("image", 50, MySqlDbType.VarChar),
 		});
@@ -525,7 +525,7 @@ namespace MySQLWrapper.Data
 		/// <param name="category">The id of a <see cref="ProductCategory"/>.</param>
 		/// <param name="name">The id of a <see cref="LanguageItem"/>.</param>
 		/// <param name="image">The id of a <see cref="Data.Image"/>.</param>
-		public Product(int? id, string manufacturer, int category, string name, string image = "default")
+		public Product(string id, string manufacturer, string category, string name, string image = "default")
 		{
 			Id = id;
 			Manufacturer = manufacturer;
@@ -535,10 +535,15 @@ namespace MySQLWrapper.Data
 		}
 
 		#region Properties
-		public int? Id
+		public string Id
 		{
-			get { return (int?)Fields[0]; }
-			set { _fields[0] = value; }
+			get { return (string)Fields[0]; }
+			set
+			{
+				if (value != null && value.Length > Metadata[0].Length)
+					throw new ArgumentException("Value exceeds the maximum length specified in the metadata.");
+				_fields[0] = value;
+			}
 		}
 		public string Manufacturer
 		{
@@ -550,10 +555,15 @@ namespace MySQLWrapper.Data
 				_fields[1] = value;
 			}
 		}
-		public int Category
+		public string Category
 		{
-			get { return (int)Fields[2]; }
-			set { _fields[2] = value; }
+			get { return (string)Fields[2]; }
+			set
+			{
+				if (value != null && value.Length > Metadata[2].Length)
+					throw new ArgumentException("Value exceeds the maximum length specified in the metadata.");
+				_fields[2] = value;
+			}
 		}
 		public string Name
 		{
