d28ed84ef1bd14a62f783174a30cd550df6ef32a
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-05-20 16:48:58 +0200

    Added criteria condition builder method

--- Web API/Misc.cs
+++ Web API/Misc.cs
@@ -1,9 +1,42 @@
-﻿using System;
+﻿using MySQLWrapper.Data;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace API {
-    class Misc {
-
+namespace API
+{
+	class Misc {
+		public static MySqlConditionBuilder CreateCondition(JObject criteria)
+		{
+			var condition = new MySqlConditionBuilder();
+			int i = 0;
+			foreach (KeyValuePair<string, JToken> pair in criteria)
+			{
+				if (i > 0) condition.And();
+				condition.NewGroup();
+				condition.Column(pair.Key);
+				string value = (string)pair.Value;
+				string[] operands = value.Split("OR");
+				foreach (string operand in operands)
+				{
+					string[] split = operand.Split(" ");
+					if (split[0] == "LIKE")
+					{
+						condition.Like(split[1]);
+					}
+					else
+					{
+						condition.Equals(operand, MySql.Data.MySqlClient.MySqlDbType.String);
+					}
+					if (operands.Last() != operand)
+					{
+						condition.Or();
+					}
+				}
+				condition.ExitGroup();
+				i++;
+			}
+			return condition;
+		}
     }
 }
