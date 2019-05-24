f35726153b2e4262b2d9a88685de7621aa3d1a77
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-05-25 00:54:38 +0200

    Added deleteLoan function

--- Web API/Requests/Loans/deleteLoanItem.cs
+++ Web API/Requests/Loans/deleteLoanItem.cs
@@ -1,4 +1,55 @@
-﻿namespace API.Requests.Loans {
-	class deleteLoanItem {
+﻿using MySql.Data.MySqlClient;
+using MySQLWrapper.Data;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using static API.Requests.RequestMethodAttributes;
+
+namespace API.Requests
+{
+	static partial class RequestMethods
+	{
+		/// <summary>
+		/// Handles requests with requestType "deleteLoan".
+		/// </summary>
+		/// <remarks>
+		/// Optional arguments are:
+		///		- Columns > a string[] specifying what fields to return. If omitted or empty, all fields will be returned.
+		///		- Start > Specifies a limit where all returned loans must have ended after this limit.
+		///		- End > Specifies a limit where all returned loans must have started before this limit.
+		/// </remarks>
+		/// <param name="request">The request from the client.</param>
+		/// <returns>The contents of the requestData field, which is to be returned to the client.</returns>
+		[verifyPermission(User.UserPermission.User)]
+		public static JObject deleteLoan(JObject request)
+		{
+			// Get arguments
+			JObject requestData = request["requestData"].ToObject<JObject>();
+			requestData.TryGetValue("loanId", out JToken loanId);
+
+			// Verify arguments
+			if (loanId == null || loanId.Type != JTokenType.Integer)
+				return Templates.InvalidArgument("loanId");
+			
+			// Build condition
+			var condition = new MySqlConditionBuilder();
+			condition.Column("user")
+				.Equals(CurrentUser.Username, MySqlDbType.String)
+				.And()
+				.Column("id")
+				.Equals(loanId, MySqlDbType.String);
+			
+			// Get and delete loan, or return error if no such loan exists
+			var loan = wrapper.Select<LoanItem>(condition, range: (0, 1)).FirstOrDefault();
+			if (loan == null)
+				return Templates.NoSuchLoan(loanId.ToString());
+			wrapper.Delete(loan);
+			
+			// Create response
+			JObject response = new JObject() {
+				{"reason", null }
+			};
+			return response;
+		}
 	}
 }
