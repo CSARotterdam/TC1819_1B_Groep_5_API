959d6e1847b60e059f2cf5e9ee166a9d2699421a
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-06-01 22:02:44 +0200

    Optimize getProductAvailability so that it uses two DB queries in total, instead of 3 per item.

--- Web API/Requests/Statistics/getProductAvailability.cs
+++ Web API/Requests/Statistics/getProductAvailability.cs
@@ -11,30 +11,39 @@ namespace API.Requests {
 		[RequiresPermissionLevel(UserPermission.Collaborator)]
 		public JObject getProductAvailability(JObject request) {
 			//Get arguments
-			request.TryGetValue("products", out JToken statTypeValue);
-			if (statTypeValue == null || (statTypeValue.Type != JTokenType.String && statTypeValue.Type != JTokenType.Array)) {
+			request.TryGetValue("products", out JToken productValue);
+			if (productValue == null || (productValue.Type != JTokenType.String && productValue.Type != JTokenType.Array)) {
 				return Templates.MissingArguments("statType");
 			}
 
 			//Parse arguments
 			List<string> productIDs = new List<string>();
-			if (statTypeValue.Type == JTokenType.String) {
+			if (productValue.Type == JTokenType.String) {
 				//TODO Allow * to be used as value, selecting all products
-				productIDs.Add(statTypeValue.ToObject<string>());
-			} else if (statTypeValue.Type == JTokenType.Array) {
-				productIDs = statTypeValue.ToObject<List<string>>();
+				productIDs.Add(productValue.ToObject<string>());
+			} else if (productValue.Type == JTokenType.Array) {
+				productIDs = productValue.ToObject<List<string>>();
 			}
 
-			//Create base response
-			JObject response = new JObject();
+			//Get all relevant loans
+			MySqlConditionBuilder query = new MySqlConditionBuilder();
+			foreach (string ID in productIDs) {
+				query.Or();
+				query.Column("id");
+				query.Equals(ID, MySqlDbType.String);
+			}
+			List<ProductItem> productItems = Connection.Select<ProductItem>(query).ToList();
+			query = new MySqlConditionBuilder();
+			foreach (ProductItem pitem in productItems) {
+				query.Or();
+				query.Column("id");
+				query.Equals(pitem.Id, MySqlDbType.String);
+			}
+			List<LoanItem> loanItems = Connection.Select<LoanItem>(query).ToList();
 
-			//Retrieve statistics
+			//Create response
+			JObject results = new JObject();
 			foreach (string productID in productIDs) {
-				//If the product doesn't exist, add an error entry
-				if (GetObject<Product>(productID) == null) {
-					response[productID] = "NoSuchProduct";
-					continue;
-				}
 
 				//Create base entry
 				JObject entry = new JObject() {
@@ -44,43 +53,34 @@ namespace API.Requests {
 					{"inStock", 0}
 				};
 
-				//Get all loans belonging to this product
-				List<ProductItem> productItems = Connection.Select<ProductItem>(new MySqlConditionBuilder()
-					.Column("product")
-					.Equals(productID, MySqlDbType.String)
-				).ToList();
-				List<LoanItem> loans = new List<LoanItem>();
-				foreach (ProductItem pitem in productItems) {
-					List<LoanItem> loanitems = Connection.Select<LoanItem>(new MySqlConditionBuilder()
-						.Column("product_item")
-						.Equals(pitem.Id, MySqlDbType.Int32)
-					).ToList();
-					loans.AddRange(loanitems);
+				//Calculate stats
+				foreach(ProductItem productitem in productItems) {
+					if(productitem.ProductId != productID) {
+						continue;
+					}
+					entry["total"] = (int)entry["total"] + 1;
+
+					foreach(LoanItem loan in loanItems) {
+						if(loan.ProductItem != productitem.Id) {
+							continue;
+						}
+						entry["reservations"] = (int)entry["reservations"] + 1;
+
+						if (loan.IsAcquired) {
+							entry["loanedOut"] = (int)entry["loanedOut"] + 1;
+						}
+					}
 				}
-
-				//Count total productItems
-				entry["total"] = productItems.Count();
-
-				//Count total reservations
-				entry["reservations"] = loans.Count;
-
-				//Count total loanedOut
-				foreach (LoanItem loan in loans) {
-					if (loan.IsAcquired)
-						entry["loanedOut"] = (int)entry["loanedOut"] + 1;
-				}
-
-				//Count total inStock
 				entry["inStock"] = (int)entry["total"] - (int)entry["loanedOut"];
 
 				//Add entry
-				response[productID] = entry;
+				results[productID] = entry;
 			}
 
 			//Return response
 			return new JObject() {
 				{"reason", null },
-				{"responseData", response}
+				{"responseData", results}
 			};
 		}
 	}
