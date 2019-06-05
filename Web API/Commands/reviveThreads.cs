fff366ce258de2e4c750f5d19ff5eb053a46260e
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-06-05 19:40:32 +0200

    Added new ReviveThreads command

---
+++ Web API/Commands/reviveThreads.cs
@@ -0,0 +1,42 @@
+﻿using API.Threads;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics;
+
+namespace API.Commands
+{
+	static partial class CommandMethods
+	{
+		[MonitoringDescription("Restarts all worker threads that are no longer running.")]
+		public static void ReviveThreads(string[] _)
+		{
+			int successes = 0;
+			int restarts = 0;
+			for (int i = 0; i < Program.RequestWorkers.Length; i++)
+			{
+				var worker = Program.RequestWorkers[i];
+				if (worker == null || !worker.IsAlive || worker.State == ConnectionState.Closed || worker.State == ConnectionState.Broken)
+				{
+					restarts++;
+					worker?.Dispose();
+					try
+					{
+						Program.RequestWorkers[i] = new RequestWorker(
+							Program.CreateConnection(),
+							Program.ListenerThread.Queue,
+							worker?.Name ?? "RequestWorker" + (i + 1),
+							log
+						);
+						Program.RequestWorkers[i].Start();
+						successes++;
+					}
+					catch (MySqlException e)
+					{
+						log.Error($"Error while creating connection for {"RequestWorker" + (i + 1)}: {e.GetType().Name}: {e.Message}", false);
+					}
+				}
+			}
+			log.Info($"Restarted {successes}/{restarts} workers");
+		}
+	}
+}
