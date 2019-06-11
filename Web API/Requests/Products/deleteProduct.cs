9f17e2eb11d3dfe711492b2afea3191170d17b0c
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-06-11 12:09:14 +0200

    SAVE US KNER, YOU'RE OUR ONLY HOPE

--- Web API/Requests/Products/deleteProduct.cs
+++ Web API/Requests/Products/deleteProduct.cs
@@ -27,24 +27,29 @@ namespace API.Requests {
 			}
 
 			// Check if items or acquired loans exist
-			var condition = new MySqlConditionBuilder("id", MySqlDbType.Int32, productID);
+			var condition = new MySqlConditionBuilder("product", MySqlDbType.String, productID);
 			var items = Connection.Select<ProductItem>(condition).ToList();
 
 			// Get associated loans
-			condition = new MySqlConditionBuilder("product_item", MySqlDbType.Int32, items.Select(x => x.Id));
+			condition = new MySqlConditionBuilder("product_item", MySqlDbType.Int32, items.Select(x => x.Id).Cast<object>().ToArray());
 			List<bool> loans_isAcquired = Connection.Select<LoanItem>(new string[] { "is_item_acquired" }, condition).Select(x => (bool)x[0]).ToList();
 
 			// Check if any loans are aquired
 			if (loans_isAcquired.Any(x => x))
 				return Templates.CannotDelete("This product still has active loans.");
 
-			// deltete stuffz
+			// Delete image
 			product.Delete(Connection);
-			if (product.Name != "default") {
+			if (product.Image != "default") {
 				product.GetImage(Connection)?.Delete(Connection);
 			}
+			// Delete name languageItem if it isnt the default languageItem
 			if (product.Name != "0") {
 				product.GetName(Connection)?.Delete(Connection);
+			}
+			// Delete description languageItem if it isnt the default languageItem
+			if (product.Description != "0") {
+				product.GetDescription(Connection)?.Delete(Connection);
 			}
 
 			//Create base response
